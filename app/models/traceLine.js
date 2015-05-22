b10a4fdff1be870458f055e866c7b4e150e9b156
gkolocsar <gkolocsar@makingsense.com>
2015-05-22 14:53:09 -0300

    Adds traceLevel toUpperCase

--- app/models/traceLine.js
+++ app/models/traceLine.js
@@ -8,13 +8,6 @@ var traceLineSchema   = new Schema({
 	serverDateTime: String
 });
 
-//var TraceLine = function (serverDateTime, traceLevel, message){
-//	var self = this;
-//	self.serverDateTime = serverDateTime;
-//	self.message = message;
-//	self.traceLevel = traceLevel.toUpperCase();
-//};
-
 var TraceLine = mongoose.model('TraceLine', traceLineSchema);
 
 TraceLine.prototype.print = function(){
